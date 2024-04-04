9c17b737b0bc6135ccc888c331d5622a47bb4c70
guilhermeileite <130417023+guilhermeileite@users.noreply.github.com>
2024-04-04 19:09:12 -0300

    Add files via upload

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,12 +1,11 @@
 /// <reference types="cypress" />
 
-import { faker } from '@faker-js/faker';
-const email = faker.internet.email()
+
 
 describe('Teste de Autenticação', () => {
     it('Teste para fluxo de checkout', () => {
         // fazendo login
-        cy.login(email, 'senha12345')
+        cy.login('guilhermeleite@ebac.com', 'senhateste12345')
         // adicionando produtos ao carrinho  
         cy.visit('http://lojaebac.ebaconline.art.br/product/aether-gym-pant/')
         cy.get('.button-variable-item-32').click()
@@ -18,6 +17,7 @@ describe('Teste de Autenticação', () => {
         cy.get('#payment_method_cod').click()
         cy.get('#terms').click()
         cy.get('#place_order').click()
+        cy.checkout('Guilherme', 'Leite', 'Microsoft', 'Avenida Paulista, 2000', 'São Paulo', '00000222', '999999999', 'guilhermeleite@ebac.com')
         cy.contains('Obrigado. Seu pedido foi recebido.').should('be.visible')
     })
 })
