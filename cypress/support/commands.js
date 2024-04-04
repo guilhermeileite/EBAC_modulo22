6d8d53abfb37a05d0ea53c1ec99b75c6b748fcd0
guilhermeileite <130417023+guilhermeileite@users.noreply.github.com>
2024-04-04 19:11:16 -0300

    Add files via upload

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,16 +24,27 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login', (email, password) => {
-    const formdata = new FormData();
-    formdata.append('username', email)
-    formdata.append('password', password)
-    formdata.append('login', 'Login')
+Cypress.Commands.add('login', (usuario, senha) => { 
+    const formdata = new FormData() 
+    formdata.append('log', usuario) 
+    formdata.append('pwd', senha) 
+    formdata.append('wp-submit', 'Acessar') 
+    formdata.append('redirect_to', `/wp-admin`) 
+    formdata.append('testcookie', 1) 
+    cy.request({ url: `/wp-login.php`, method: "POST", body: formdata }) 
+    cy.visit('minha-conta') 
 
-    cy.request({
-        url: 'http://lojaebac.ebaconline.art.br/minha-conta/',
-        method: 'POST',
-        body: formdata
-    })
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-})
+});
+
+Cypress.Commands.add('checkout', (nome, sobrenome, empresa, endereco, cidade, cep, telefone, email) => { 
+    cy.get('#billing_first_name').type(nome)
+        cy.get('#billing_last_name').type(sobrenome)
+        cy.get('#billing_company').type(empresa)
+        cy.get('#select2-billing_country-container')
+        cy.get('#billing_address_1').type(endereco)
+        cy.get('#billing_city').type(cidade)
+        cy.get('#billing_postcode').type(cep)
+        cy.get('#billing_phone').type(telefone)
+        cy.get('#billing_email').type(email)
+        cy.get('#place_order').click()
+});
