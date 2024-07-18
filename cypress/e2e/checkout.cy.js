2b66499666873c49ab4904b9d8a1fa0ff5781dcd
guilhermeileite <guilhermekpr@gmail.com>
2024-07-17 21:36:08 -0300

    Refazendo o fluxo de teste com App Actions

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -3,6 +3,7 @@ describe('Exercicio módulo 23', () => {
       cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
       cy.get('#reg_email').type('guilhermeleite@ebac.com')
       cy.get('#reg_password').type('Aluno@ebac2024')  
+      cy.visit('http://lojaebac.ebaconline.art.br/product/ingrid-running-jacket')
       cy.addItemInCart2('Ingrid Running Jacket', 'XS', 'Blue', 2, '2559', '2559', '2580')
       cy.visit('http://lojaebac.ebaconline.art.br/carrinho/')
       cy.get('.checkout-button').click()
